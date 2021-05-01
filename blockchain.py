30f84d42a63c3d405fe4aa4d5547ffe23202c8b5
vidumini <heartyvds@gmail.com>
2021-05-02 00:09:35 +0530

    updated methods

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,19 @@
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.transactions = []
+        self.current_transactions = []
 
     def new_block(self, proof):
         block = {
           'index': len(self.chain) + 1,
           'timstamp': time(),
-          'transactions': self.transactions,
+          'transactions': self.current_transactions,
           'proof': proof,
           'previous_hash': self.hash(self.last_block())
         }
+        self.chain.append(block)
+        self.current_transactions = []
+        return block
         pass
 
     def new_transaction(self, sender, recipient, amount):
@@ -19,8 +22,8 @@ class Blockchain:
           'recipient': recipient,
           'amount': amount
         }
-        self.transactions.append(transaction)
-        return self.last_block()
+        self.current_transactions.append(transaction)
+        return self.last_block['index'] + 1
         pass
 
     @staticmethod
