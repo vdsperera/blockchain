f2a6f62ec0877349ff95c5267ec0e80860810438
vidumini <heartyvds@gmail.com>
2021-05-03 15:16:39 +0530

    updated methods

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,9 @@
+from time import time
+import hashlib
+from uuid import uuid4
+import json
+from flask import Flask
+
 class Blockchain:
     def __init__(self):
         self.chain = []
@@ -26,6 +32,19 @@ class Blockchain:
         return self.last_block['index'] + 1
         pass
 
+    def proof_of_work(self, previous_proof):
+      check_proof = 0
+      while self.valid_proof(previous_proof, check_proof) is False:
+        check_proof = check_proof + 1
+      return check_proof
+      pass
+
+    def valid_proof(previous_proof, check_proof):
+      guess_answer = f'{previous_proof}{check_proof}'.encode()
+      guess_hash = hashlib.sha256(guess_answer).hexdigest()
+      return guess_hash[:4] == '0000'
+      pass
+
     @staticmethod
     def hash(block):
         pass
@@ -35,3 +54,36 @@ class Blockchain:
         return self.chain[-1]
         pass
     
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+  # request = {
+  #   'sender': '',
+  #   'recipient': '',
+  #   'amount': ''
+  # }
+  values = request.get_json()
+  required = ['sender', 'recipient', 'amount']
+  if not all(for value in values:)
+  pass
+
+@app.route('/mine', methods=['GET'])
+def mine():
+  pass
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+  response = {
+    'chain': blockchain.chain,
+    'length': len(blockchain.chain)
+  }
+  return response
+  pass
+
+if __name__ == '__main__':
+  app.run(host='0:0:0:0', port=5000)
